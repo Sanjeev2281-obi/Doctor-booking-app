71068285d3974d97abfde18f825773e647c1be1f
Sanjeev2281-obi <sanjeevasas2281@gmail.com>
2025-07-02 15:44:27 +0530

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  theme:{
-    extend:{
-      color:{
-        primary:"#5f6FFF",
-      }
-    },
-     build: {
+  plugins: [react(), tailwindcss()],
+  base: './',
+  build: {
     rollupOptions: {
       external: ['react-router-dom']
     }
-  }
   }
 })
